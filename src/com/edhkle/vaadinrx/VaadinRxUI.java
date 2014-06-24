41bf99752b318639fc69ce51b214c75501a5ab1f
Erik Hansen <ehansen@parallelwireless.com>
2014-06-24 18:11:31 -0400

    Working Demo

--- src/com/edhkle/vaadinrx/VaadinRxUI.java
+++ src/com/edhkle/vaadinrx/VaadinRxUI.java
@@ -1,37 +1,23 @@
 package com.edhkle.vaadinrx;
 
-import java.sql.SQLException;
-import java.util.Collection;
-import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.servlet.annotation.WebServlet;
 
-import com.edhkle.pocketrx.controller.Pharmacist;
-import com.edhkle.pocketrx.model.Pill;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.data.util.BeanContainer;
 import com.vaadin.server.Page;
-import com.vaadin.server.Resource;
-import com.vaadin.server.Sizeable;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Accordion;
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.HorizontalSplitPanel;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.ListSelect;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.Table;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+import com.vaadin.ui.themes.Reindeer;
 
 @SuppressWarnings("serial")
 @Theme("vaadinrx")
@@ -51,7 +37,6 @@ public class VaadinRxUI extends UI {
 	@Override
 	protected void init(VaadinRequest request) {
 		try {
-			//p = Pharmacist.getPharmacist(JDBC_URL);			
 			initLayout();
 		} catch (Exception e) {
 			showError(e);
@@ -59,60 +44,12 @@ public class VaadinRxUI extends UI {
 	}
 
 	protected void initLayout() throws Exception {
-		final VerticalLayout mainLayout = new VerticalLayout();
-		mainLayout.setMargin(true);
-		setContent(mainLayout);
-		
-		Panel mainPanel = new Panel("VaadinRX Demo Application");
-		mainPanel.setIcon(new ThemeResource("../images/pill.png"));
-//		mainPanel.setHeight("800");
-//		mainPanel.setWidth("600");
-		mainLayout.addComponent(mainPanel);
-		mainPanel.setContent(new MainUI());
-//		HorizontalSplitPanel split = new HorizontalSplitPanel();
-//		mainPanel.setContent(split);
-//		final VerticalLayout leftPanel = new VerticalLayout();
-//		final VerticalLayout rightPanel = new VerticalLayout();
-//		split.setSplitPosition(15, Sizeable.UNITS_PERCENTAGE);
-//		split.setLocked(true);
-//		split.setWidth("100%");
-//		split.setFirstComponent(leftPanel);
-//		split.setSecondComponent(rightPanel);
-//		
-//		initTabsheet();
-//		mainPanel.setContent(tabsheet);
-//		Button button = new Button("Load Records");
-//		button.setWidth("100%");
-//		button.addClickListener(new Button.ClickListener() {
-//			public void buttonClick(ClickEvent event) {
-//				try {
-//					log.info("Accordian caption=" + tabsheet.getCaption() + " selected tab=" + tabsheet.getSelectedTab() + " selectedTabCaption=" + tabsheet.getSelectedTab().getCaption() + " tabDesc=" + tabsheet.getSelectedTab().getDescription());
-//					updateResultsPanel();
-//				} catch (Exception e) {
-//					Notification n = new Notification(e.getClass().getSimpleName(), "<br/><i>" + e.getMessage() + "</i>", Notification.TYPE_ERROR_MESSAGE, true);
-//					n.show(Page.getCurrent());					
-//				}
-//			}
-//		});
-
-//		leftPanel.addComponent(button);		
-//		leftPanel.setComponentAlignment(button, Alignment.BOTTOM_CENTER);
-//		
-//		Panel resultsPanel = new Panel("Selected Results");
-//		rightPanel.addComponent(resultsPanel);
-//		updateResultsPanel();
-//		resultsPanel.setContent(resultsTable);
-	}
-	
-//	protected void initTabsheet() throws SQLException {
-//		tabsheet.setSizeFull();
-//		Map<String,Integer> aMap = p.getAlphaList();
-//		for(Map.Entry<String,Integer> entry : aMap.entrySet()) {
-//			log.info("Adding tabsheet tab: Key[" + entry.getKey() + "] = " + entry.getValue());
-//			tabsheet.addTab(new Label(entry.getKey() + ": (" + entry.getValue() + " records)"), entry.getKey());
-//		}
-//	}
-//	
+		Panel mainWindow = new Panel("VaadinRX Demo Application");
+		mainWindow.setStyleName(Reindeer.LAYOUT_BLUE);
+		mainWindow.setIcon(new ThemeResource("../images/pill.png"));
+		setContent(mainWindow);
+		mainWindow.setContent(new MainUI());
+    }
 
 	public static void showError(Throwable t) {
 		Notification n = new Notification(t.getClass().getSimpleName(), "<br/><i>" + t.getMessage() + "</i>", Notification.TYPE_ERROR_MESSAGE, true);
