5d417775ee7809df517b61ab4389e893ae3dbf42
Erik Hansen <ehansen@parallelwireless.com>
2014-06-26 10:57:22 -0400

    Working Demo

--- src/com/edhkle/vaadinrx/VaadinRxUI.java
+++ src/com/edhkle/vaadinrx/VaadinRxUI.java
@@ -7,17 +7,14 @@ import javax.servlet.annotation.WebServlet;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.Page;
-import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Notification;
-import com.vaadin.ui.Panel;
 import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
-import com.vaadin.ui.themes.Reindeer;
+import java.util.logging.Level;
 
 @SuppressWarnings("serial")
 @Theme("vaadinrx")
@@ -44,16 +41,16 @@ public class VaadinRxUI extends UI {
 	}
 
 	protected void initLayout() throws Exception {
-		Panel mainWindow = new Panel("VaadinRX Demo Application");
-		mainWindow.setStyleName(Reindeer.LAYOUT_BLUE);
-		mainWindow.setIcon(new ThemeResource("../images/pill.png"));
-		setContent(mainWindow);
-		mainWindow.setContent(new MainUI());
+            Window mainWindow = new Window("VaadinRX Demo Application");
+            mainWindow.setContent(new NewUI());
+            mainWindow.setWidth("100.0%");
+	    addWindow(mainWindow);
     }
 
 	public static void showError(Throwable t) {
 		Notification n = new Notification(t.getClass().getSimpleName(), "<br/><i>" + t.getMessage() + "</i>", Notification.TYPE_ERROR_MESSAGE, true);
-		n.show(Page.getCurrent());		
+		n.show(Page.getCurrent());
+                log.log(Level.SEVERE, t.getClass().getName() + ": " + t.getMessage(), t);
 	}	
 	public static void showWarning(String caption, String description) {
 		Notification n = new Notification(caption, description, Notification.TYPE_WARNING_MESSAGE, true);
